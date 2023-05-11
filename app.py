45f3068bbcd0688291ede9d81ded0a49aa8af6b5
Guru Charan <56435472+GuruCharanKakaraparthi@users.noreply.github.com>
2023-05-10 21:01:15 -0500

    app.py

--- app.py
+++ app.py
@@ -40,6 +40,38 @@ def first():
     # Render the search form
     return render_template("allquestions.html")
 
+
+@app.route("/bonus", methods=["GET", "POST"])
+def bonus():
+    if request.method == "POST":
+        # Retrieve form data
+        state_name = request.form.get("state_name")
+        min_population = request.form["min_population2"]
+        max_population = request.form["max_population2"]
+
+        # Construct SQL query
+        query = "SELECT city, state, population, lat, lon FROM city WHERE state = %s AND population BETWEEN %s AND %s"
+        params = (state_name, min_population, max_population)
+
+        # Execute SQL query
+        cursor = db.cursor()
+        cursor.execute(query, params)
+
+        # Fetch results
+        results = cursor.fetchall()
+
+        # Render template with results
+        return render_template("first.html", results=results)
+
+    # Render the search form
+    return render_template("allquestions.html")
+
+
+
+
+
+
+
 @app.route("/basic")
 def index():
     return "<h1>Hello Azure!</h1>"
