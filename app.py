4cc51d7d2c4a0c5c56ac6f6a55472bc5e9febc20
Guru Charan <56435472+GuruCharanKakaraparthi@users.noreply.github.com>
2023-05-10 20:52:05 -0500

    app.py

--- app.py
+++ app.py
@@ -6,13 +6,39 @@ app.permanent_session_lifetime=timedelta(minutes=10)
 app = Flask(__name__,template_folder="templates",static_folder="static")
 app.secret_key="manbigdat"
 # Connect to PHPMyAdmin cloud database
-connection = pymysql.connect(
+db = pymysql.connect(
     host='sql9.freesqldatabase.com',
     user='sql9601859',
     password='4EmEWs8HSl',
     db='sql9601859'
 
 )
+@app.route("/first", methods=["GET", "POST"])
+def first():
+    if request.method == "POST":
+        # Retrieve form data
+        min_lat = request.form["min_lat"]
+        max_lat = request.form["max_lat"]
+        min_population = request.form["min_population"]
+        max_population = request.form["max_population"]
+
+        # Construct SQL query
+        query = "SELECT city, State, Population, lat, lon FROM city WHERE lat BETWEEN %s AND %s AND Population BETWEEN %s AND %s"
+        params = (min_lat, max_lat, min_population, max_population)
+
+        # Execute SQL query
+        cursor = db.cursor()
+        cursor.execute(query, params)
+
+        # Fetch results
+        results = cursor.fetchall()
+        print(results)
+
+        # Render template with results
+        return render_template("first.html", results=results)
+
+    # Render the search form
+    return render_template("allquestions.html")
 
 @app.route("/basic")
 def index():
